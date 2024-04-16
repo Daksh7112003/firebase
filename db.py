90ea90a4bf85bce59d6e0a2777a3c347f8953306
Daksh <115346924+Daksh7112003@users.noreply.github.com>
2024-04-16 13:28:46 +0530

    Update db.py

--- db.py
+++ db.py
@@ -2,17 +2,7 @@ import pyrebase
  
 
 firebaseConfig = {
-#     'apiKey': "AIzaSyCjlB82mH47x_7-BRkBZB2KBQWw9Ei6VhA",
-#   'authDomain': "daksh-3e20b.firebaseapp.com",
 
-# #   'databaseURL': "https://realtimedbstorage-4249a-default-rtdb.firebaseio.com",
-
-
-#   'projectId': "daksh-3e20b",
-#   'storageBucket': "daksh-3e20b.appspot.com",
-#   'messagingSenderId': "428124117760",
-#   'appId': "1:428124117760:web:d618f8c329b6fa51c5b1e5",
-#   'measurementId': "G-QHSPMX7NTQ"
 
 
 
